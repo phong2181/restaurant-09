ec55040f3ae07b6be97d74ef8a84c3d7d9e8e33d
phong2181 <vanphong2131@gmail.com>
2025-07-24 12:45:49 +0700

    Update build & export site

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: F:\NextJS\project_restauran_09\src\app\page.tsx
+// File: F:\NextJS\project_restaurant_09\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
